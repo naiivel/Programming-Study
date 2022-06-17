7d2d5b84930f7c9c1e1dc3d08803e3004d52c708
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-17 17:01:47 +0900

    Create Operator00ArithmaeticTest.java

---
+++ Java/02_operators/Operator00ArithmaeticTest.java
@@ -0,0 +1,38 @@
+public class Operator00ArithmaeticTest {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		//양의 정수 표현 : 생략가능
+		System.out.println(+num1);
+		//음의 정수 표현 
+		System.out.println(-num2);
+		System.out.println(num2);
+		num2 = -num2;
+		System.out.println(num2);
+		
+		num1 = 10;
+		num2 = 3;
+		int result = num1 / num2;
+		System.out.println("result : " + result);
+		
+		double dResult = num1 / num2;
+		System.out.println("dResult : " + dResult);
+		double typeResult = num1 /dResult;
+		System.out.println("typeResult : " + typeResult);
+		String s = "최기근";
+		s = num1 +num2 + s;
+		System.out.println(s);
+		
+		int a = 1;
+		int b = 1;
+		int c = a++;
+		System.out.println("c : " + c);
+		System.out.println("a : " + c);
+		c = ++a;
+		System.out.println("a : " + c);
+		
+		
+	}
+
+}
