71454efcccd3c2956150d0e5f710e3fab32b6386
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-17 17:10:43 +0900

    Create Field.java

---
+++ Java/07_class/field/Field.java
@@ -0,0 +1,15 @@
+package b.field;
+
+public class Field {
+	//attribute 속성
+	//property 소유물,재산
+	//field 실제 메모리에 올라가는 공간
+	//전역변수(class block 전체에서 사용가능한 변수)
+	//객체,인스턴스 멤버 변수
+	
+	int i;
+	boolean b; 
+	double d;
+	String s;
+	
+}
