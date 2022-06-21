2e7e2b06717bb332cdeb5e489221dcf2efc332fb
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-21 16:55:44 +0900

    Add files via upload

---
+++ Java/07_class/reference/CarExample.java
@@ -0,0 +1,30 @@
+package e.reference;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		System.out.println(car.company);
+		System.out.println(car.model);
+		System.out.println(car.maxSpeed);
+		System.out.println(car.engine);
+		
+		car.company = "현대자동차";
+		car.model = "G70";
+		car.maxSpeed = 250;
+		
+		Engine engine = new Engine();
+		car.engine = engine;
+		
+		System.out.println(engine == car.engine);
+		car.engine.rpm = 2000;
+		System.out.println(engine.rpm);
+		
+		Car car2 = new Car();
+		car2.company = "KIA";
+		car2.model = "모닝";
+		car2.engine = new Engine();
+		car2.engine = engine;
+		System.out.println(car2.engine.rpm);
+	}
+}
