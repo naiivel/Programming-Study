1b8271e4af11c2bebd027806bcba7f7ff54b056c
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-21 17:07:57 +0900

    Add files via upload

---
+++ Java/11_extends/extends2/phone/DMBPhone.java
@@ -0,0 +1,29 @@
+package extends2.phone;
+
+public class DMBPhone extends Phone{
+	
+	int channel;
+	
+	public DMBPhone() {}
+
+	//alt + s + a
+	public DMBPhone(String model, String color, int channel) {
+		super(model, color);
+		/*
+		super.model = model; //가능
+		this.color = color; //가능
+		*/
+		this.channel = channel;
+	}
+	
+	void turnOnDMB() {
+		System.out.println("DMB를 켭니다.");
+	}
+	void turnOff() {
+		System.out.println("DMB를 끕니다.");
+	}
+	void changeChannel(int channel) {
+		this.channel = channel;
+		System.out.println("채널이 " + this.channel + "로 변경되었습니다.");
+	}
+}
