7e666d399f17b3485136ca3bf8cdb7e2e02faff6
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-21 16:52:36 +0900

    Create Member.java

---
+++ JSP/10_useBean/Member.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Member {
+	
+	private String name = "최기근";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
