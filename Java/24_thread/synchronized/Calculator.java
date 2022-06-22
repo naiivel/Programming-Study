00fb32e94c32648cf2ea3c6ffa89b50485136d12
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-22 17:17:08 +0900

    Create Calculator.java

---
+++ Java/24_thread/synchronized/Calculator.java
@@ -0,0 +1,24 @@
+package t04_synchronized;
+
+public class Calculator {
+	
+	private int memory;
+	
+	public int getMemory() {
+		return memory;
+	}
+	
+	//public synchronized void setMemory(int memory) {
+	public void setMemory(int memory) {
+		synchronized (this) {
+			this.memory = memory;
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {}
+		//현재 setMemory를 사용중인 스레드 정보 출력
+		String name = Thread.currentThread().getName();
+		System.out.println(name+" : "+this.memory);
+		}
+	}
+	
+}
