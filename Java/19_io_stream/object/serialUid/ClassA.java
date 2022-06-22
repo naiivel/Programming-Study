601e05125fc0da166d5efa6b8e92de16742adfd3
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-22 17:03:22 +0900

    Create ClassA.java

---
+++ Java/19_io_stream/object/serialUid/ClassA.java
@@ -0,0 +1,11 @@
+package f09_object.serialUid;
+
+import java.io.Serializable;
+
+public class ClassA implements Serializable{
+	
+	private static final long serialVersionUID = 7275854467370701470L;
+	int filed1;
+	int filed2;
+	
+}
