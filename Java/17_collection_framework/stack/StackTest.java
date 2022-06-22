d53620aa9fdb5b25dfb5044f587b146f68338483
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-22 16:55:57 +0900

    Add files via upload

---
+++ Java/17_collection_framework/stack/StackTest.java
@@ -0,0 +1,24 @@
+package c5_stack;
+
+public class StackTest {
+
+	public static void methodA() {
+		System.out.println("methodA 호출");
+		int result = methodB(5);
+		System.out.println("result : " + result);
+		System.out.println("methodA 종료");
+	}
+	
+	public static int methodB(int b) {
+		System.out.println("methodB 호출");
+		int result = b*b;
+		System.out.println("methodB 종료");
+		return result;
+	}
+	
+	public static void main(String[] args) {     //stack메소드는 후입선출 
+		System.out.println("Main 시작");
+		methodA();
+		System.out.println("Main 종료");
+	}
+}
