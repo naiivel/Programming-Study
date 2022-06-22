94e08e26c429dc408ea9d0f1f917fecf0c19db27
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-22 17:17:20 +0900

    Add files via upload

---
+++ Java/24_thread/synchronized/SynchronizedExample.java
@@ -0,0 +1,33 @@
+package t04_synchronized;
+
+class UserThread extends Thread{
+	private Calculator calc;
+	private int value;
+	
+	public void setCalculator(Calculator calc, int value) {
+		this.calc = calc;
+		this.value = value;
+	}
+	
+	@Override
+	public void run() {
+		calc.setMemory(value);
+	}
+}
+
+public class SynchronizedExample {
+
+	public static void main(String[] args) {
+		
+		Calculator calc = new Calculator();
+		UserThread u1 = new UserThread();
+		u1.setName("USER-1");
+		u1.setCalculator(calc, 100);
+		u1.start();
+		
+		UserThread u2 = new UserThread();
+		u2.setName("USER-2");
+		u2.setCalculator(calc, 50);
+		u2.start();
+	}
+}
---
+++ Java/24_thread/synchronized/VectorExample.java
@@ -0,0 +1,45 @@
+package t04_synchronized;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class VectorExample {
+
+	public static void main(String[] args) {
+		System.out.println("MAIN 시작");
+		//List<String> array = new ArrayList<>();
+		List<String> array = new Vector<>();
+		
+		Thread t1 = new Thread() {
+			public void run() {
+				for(int i=0; i<10000; i++) {
+					array.add("최기근");
+				}
+				System.out.println("t1종료");
+			}
+		};
+		t1.start();
+		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i=0; i<10000; i++) {
+					array.add("천재");
+				}
+				System.out.println("t2종료");
+			}
+		});
+		t2.start();
+		
+		while(true) {
+			if(t1.getState() == Thread.State.TERMINATED && t2.getState() == Thread.State.TERMINATED) {
+				System.out.println("t1,t2 작업 종료");
+				System.out.println("array : "+array.size());
+				break;
+			}
+		}
+		System.out.println("MAIN 종료");
+	}
+	
+}
