373f0ab4136f11598819c09fa3810b23259bf24c
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-22 16:51:48 +0900

    Create ArraysCopyOfRangeExample.java

---
+++ Java/16_Practice/api_class/ex2/ArraysCopyOfRangeExample.java
@@ -0,0 +1,19 @@
+package api_class.ex2;
+
+import java.util.Arrays;
+
+public class ArraysCopyOfRangeExample {
+
+	public static void main(String[] args) {
+		
+		int[] array1 = {10,20,30,40,50};
+		int[] array2;
+		array2 = Arrays.copyOfRange(array1, 1, 3);
+		System.out.println(Arrays.toString(array2));
+		
+		array2 = new int[2];
+		// arraycopy(원본배열, 복사 시작할 인덱스, 복사할 배열, 복사할 배열에 값이 들어갈 인덱스,복사할 길이) 
+		System.arraycopy(array1, 1, array2, 0, 2);
+		System.out.println(Arrays.toString(array2));
+	}
+}
