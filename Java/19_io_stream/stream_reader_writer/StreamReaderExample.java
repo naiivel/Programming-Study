79c1e2ee065ee6f82cc71e117f9db245faecbbbb
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-22 17:01:38 +0900

    Add files via upload

---
+++ Java/19_io_stream/stream_reader_writer/StreamReaderExample.java
@@ -0,0 +1,27 @@
+package f06_stream_reader_writer;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class StreamReaderExample {
+
+	public static void main(String[] args) {
+
+		try {
+			FileInputStream fis = new FileInputStream("C:\\Temp\\fos.txt");
+			Reader reader = new InputStreamReader(fis);
+			char[] cbuf = new char[100];
+			int readData = reader.read(cbuf);
+			System.out.println(readData);
+			System.out.println(new String(cbuf));
+			reader.close();
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
