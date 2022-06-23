72cfb8bc8f6530fe65830df2b44b5ceb5201e83b
naiivel <98449486+naiivel@users.noreply.github.com>
2022-06-23 16:43:19 +0900

    Add files via upload

---
+++ Java/24_thread/control_method/notify_wait/DataBox.java
@@ -0,0 +1,31 @@
+package t05_control_method.notify_wait;
+
+public class DataBox {
+	
+	private String data;
+
+	public synchronized String getData() {
+		if(this.data == null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {}
+		}
+		String value = this.data;
+		this.data = null;
+		System.out.println("읽은 데이터 : " + value);
+		notify();
+		return value;
+	}
+
+	public synchronized void setData(String data) {
+		if(this.data != null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {}
+			this.data = data;
+			System.out.println("생성한 데이터 : " + data);
+			notify();
+		}
+	}
+	
+}
